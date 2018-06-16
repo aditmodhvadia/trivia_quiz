13a78f28170d6087fb6c9510d8b2d10e6be3f9c1
Adit Modhvadia <adit.modhvadia@gmail.com>
2018-06-17 02:09:44 +0530

    improve sign-in, outline border bug exists

--- lib/pages/sign_in.dart
+++ lib/pages/sign_in.dart
@@ -12,6 +12,7 @@ class SignInPage extends State<signInPage> {
   static final TextEditingController _user = new TextEditingController();
   static final TextEditingController _pass = new TextEditingController();
   String res = 'Pending';
+  String _errmsg = '';
 
   @override
   Widget build(BuildContext context) {
@@ -41,33 +42,59 @@ class SignInPage extends State<signInPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              new TextField(
-                controller: _user,
-                decoration: new InputDecoration(
+              new Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: new TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  style: new TextStyle(color: Colors.white, fontSize: 22.0),
+                  controller: _user,
+                  decoration: new InputDecoration(
                     icon: new Icon(Icons.email),
-                    hintText: 'Enter Registered Email ID'),
-              ),
-              new TextField(
-                controller: _pass,
-                decoration: new InputDecoration(
-                  icon: new Icon(Icons.vpn_key), hintText: 'Enter Password'),
-                  obscureText: true,
+                    labelText: 'Email ID',
+                    labelStyle:
+                        new TextStyle(color: Colors.white, fontSize: 18.0),
+                    border: OutlineInputBorder(
+                      borderRadius:
+                          new BorderRadius.all(const Radius.circular(14.0)),
+                    ),
+                  ),
+                ),
               ),
               new Padding(
-                padding: const EdgeInsets.only(
-                  left: 36.0,
-                  top: 8.0,
-                  right: 36.0
+                padding: const EdgeInsets.all(18.0),
+                child: new TextField(
+                  controller: _pass,
+                  keyboardType: TextInputType.text,
+                  style: new TextStyle(color: Colors.white, fontSize: 22.0),
+                  decoration: new InputDecoration(
+                    icon: new Icon(Icons.vpn_key),
+                    //errorText: _errmsg, //add error text here after resolving the bug of red outline
+                    labelText: 'Password',
+                    labelStyle:
+                        new TextStyle(color: Colors.white, fontSize: 18.0),
+                    border: OutlineInputBorder(
+                      borderRadius:
+                          new BorderRadius.all(const Radius.circular(14.0)),
+                    ),
+                  ),
+                  obscureText: true,
                 ),
+              ),
+              new Padding(
+                padding:
+                    const EdgeInsets.only(left: 36.0, top: 8.0, right: 36.0),
                 child: new RaisedButton(
-                  onPressed: () {
-                    print('Sign In pressed');
-                    setState(() {
-                      //Testing onpress works and gets email id and password
-                        res = _user.text +' ' +_pass.text;
+                    onPressed: () {
+                      print('Sign In pressed');
+                      setState(() {
+                        //Testing onpress works and gets email id and password
+                        res = _user.text + ' ' + _pass.text;
+                        if (_pass.text.length < 6) {
+                          _errmsg = 'Password should be more than 6 characters';
+                        }
                         c = Colors.green[500];
-                    });
-                },
+                      });
+                    },
                     color: main.primaryColor,
                     elevation: 4.0,
                     splashColor: Colors.blueAccent,
@@ -79,17 +106,17 @@ class SignInPage extends State<signInPage> {
                         child: Text(
                           'Sign-In',
                           textAlign: TextAlign.center,
-                          style:
-                          TextStyle(color: Colors.black, fontSize: 30.0),
+                          style: TextStyle(color: Colors.black, fontSize: 30.0),
                         ),
                       ),
-                    )
-                ),
+                    )),
               ),
-              new Center(
-                child: new Text(
-                  res,
-                  textAlign: TextAlign.center,
+              new Padding(
+                padding: const EdgeInsetsDirectional.only(top: 18.0),
+                child: new Center(
+                  child: new Text(
+                    res,
+                  ),
                 ),
               )
             ],
