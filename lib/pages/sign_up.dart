67e344722a2443453ad9a4e1271ad38c08a25d76
Adit Modhvadia <adit.modhvadia@gmail.com>
2018-06-21 01:34:38 +0530

    add new fields to sign up, including snackbar for ref

--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -7,10 +7,12 @@ class signUpPage extends StatefulWidget {
 }
 
 class SignUpPage extends State<signUpPage> {
-  String _mymsg = 'Welcome';
 
   static final TextEditingController _user = new TextEditingController();
   static final TextEditingController _pass = new TextEditingController();
+  static final TextEditingController _name = new TextEditingController();
+  static final TextEditingController _phone = new TextEditingController();
+  static final TextEditingController _conpass = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +20,23 @@ class SignUpPage extends State<signUpPage> {
       appBar: AppBar(
         elevation: 6.0,
         actions: <Widget>[
-          new IconButton(
+          new Builder(
+            builder: (BuildContext context1){
+              return new IconButton(
               icon: new Icon(Icons.help),
               onPressed: () {
-                print('Help clicked');
-                setState(() {
-                  _mymsg = 'Pressed Help';
-                });
-              }),
+              print('Help clicked');
+              final _helpsnackbar = SnackBar(
+              content: Text('Help Pressed'),
+              action: SnackBarAction(
+              label: 'OK!',
+              onPressed: () => print('OK Pressed'),
+              ),
+              );
+              Scaffold.of(context1).showSnackBar(_helpsnackbar);
+              });
+            },
+          ),
         ],
         title: Text(
           'Sign-Up',
@@ -36,60 +47,157 @@ class SignUpPage extends State<signUpPage> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          new Row(
-            children: <Widget>[
-              new Padding(
-                padding: const EdgeInsets.all(14.0),
-                child: new Icon(
-                  Icons.email,
-                  size: 32.0,
-                ),
-              ),
-              new Expanded(
-                child: new Padding(
-                  padding: const EdgeInsets.only(top: 7.0, right: 28.0),
-                  child: new TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: new InputDecoration(
-                        labelText: 'Email ID',
-                        helperText: '*Required',
-                        border: OutlineInputBorder(
-                          borderRadius:
-                              new BorderRadius.all(const Radius.circular(14.0)),
-                        )),
+          new Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new Row(
+              children: <Widget>[
+                new Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: new Icon(
+                    Icons.person,
+                    size: 32.0,
                   ),
                 ),
-              ),
-            ],
+                new Expanded(
+                  child: new Padding(
+                    padding: const EdgeInsets.only(top: 7.0, right: 28.0),
+                    child: new TextField(
+                      keyboardType: TextInputType.text,
+                      controller: _name,
+                      decoration: new InputDecoration(
+                          labelText: 'Full Name',
+                          hintText: 'Enter your Full Name',
+                          border: OutlineInputBorder(
+                            borderRadius:
+                            new BorderRadius.all(const Radius.circular(14.0)),
+                          )),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-          new Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              new Padding(
-                padding: const EdgeInsets.all(14.0),
-                child: new Icon(
-                  Icons.vpn_key,
-                  size: 32.0,
-                ),
-              ),
-              new Expanded(
-                child: new Padding(
-                  padding: const EdgeInsets.only(top: 7.0, right: 28.0),
-                  child: new TextField(
-                    obscureText: true,
-                    controller: _pass,
-                    decoration: new InputDecoration(
-                        labelText: 'Password',
-                        hintText: 'Minimum 6 characters',
-                        border: OutlineInputBorder(
-                          borderRadius:
-                              new BorderRadius.all(const Radius.circular(14.0)),
-                        )),
+          new Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new Row(
+              children: <Widget>[
+                new Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: new Icon(
+                    Icons.phone,
+                    size: 32.0,
                   ),
                 ),
-              ),
-            ],
+                new Expanded(
+                  child: new Padding(
+                    padding: const EdgeInsets.only(top: 7.0, right: 28.0),
+                    child: new TextField(
+                      keyboardType: TextInputType.phone,
+                      controller: _phone,
+                      decoration: new InputDecoration(
+                          labelText: 'Contact Number',
+                          border: OutlineInputBorder(
+                            borderRadius:
+                            new BorderRadius.all(const Radius.circular(14.0)),
+                          )),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          new Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new Row(
+              children: <Widget>[
+                new Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: new Icon(
+                    Icons.email,
+                    size: 32.0,
+                  ),
+                ),
+                new Expanded(
+                  child: new Padding(
+                    padding: const EdgeInsets.only(top: 7.0, right: 28.0),
+                    child: new TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      controller: _user,
+                      decoration: new InputDecoration(
+                          labelText: 'Email ID',
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                new BorderRadius.all(const Radius.circular(14.0)),
+                          )),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          new Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                new Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: new Icon(
+                    Icons.vpn_key,
+                    size: 32.0,
+                  ),
+                ),
+                new Expanded(
+                  child: new Padding(
+                    padding: const EdgeInsets.only(top: 7.0, right: 28.0),
+                    child: new TextField(
+                      obscureText: true,
+                      controller: _pass,
+                      decoration: new InputDecoration(
+                          labelText: 'Password',
+                          hintText: 'Minimum 6 characters',
+                          border: OutlineInputBorder(
+                            borderRadius:
+                            new BorderRadius.all(const Radius.circular(14.0)),
+                          )),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          new Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                new Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: new Icon(
+                    Icons.vpn_key,
+                    size: 32.0,
+                  ),
+                ),
+                new Expanded(
+                  child: new Padding(
+                    padding: const EdgeInsets.only(top: 7.0, right: 28.0),
+                    child: new TextField(
+                      obscureText: true,
+                      controller: _conpass,
+                      decoration: new InputDecoration(
+                          labelText: 'Confirm Password',
+                          hintText: 'Minimum 6 characters',
+                          border: OutlineInputBorder(
+                            borderRadius:
+                            new BorderRadius.all(const Radius.circular(14.0)),
+                          )),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
           new Padding(
             padding: const EdgeInsets.all(18.0),
